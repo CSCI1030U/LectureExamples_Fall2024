07736e023ce889542d3b2c01df65ec2ea2d14ce3
randyfortier <randy.fortier@uoit.ca>
2024-09-05 19:36:52 -0400

    Added expressions/errors for section 006

---
+++ section006/01b_basics_expressions_errors.py
@@ -0,0 +1,33 @@
+student_age = 17
+some_ratio = 3.1415926
+alpha = 'abcdef'
+
+# account_num = input('What is your account number? ')
+account_num = '543'
+print('Account num: ' + account_num)
+print('Student age: ' + str(student_age))
+
+message = f'Student age: {student_age}'
+print(message)
+
+print(f'Student age: {student_age}')
+print(f'{student_age = }')
+
+print(f'Pi rounded is {some_ratio:.2f}.')
+
+print(f'{type(student_age)}')
+
+is_hungry = True 
+need_to_go_out = False
+print(is_hungry and need_to_go_out)
+print(f'{is_hungry or need_to_go_out=}')
+
+y = 2
+x = 1 + 8 / y
+
+a = 5
+b = 7
+c = 9
+print(f'{a=} {b=} {c=}')
+avg = (a + b - c) / 3
+print(f'{avg = }')
